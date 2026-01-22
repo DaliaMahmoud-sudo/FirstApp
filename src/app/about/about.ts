a6e126a83ffaac756449c712fa9c68af2f0e6bc3
DaliaMahmoud-sudo <dalia.m3544214@gmail.com>
2026-01-22 16:12:42 +0200

    update and delete

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Task } from '../navbar/task';
 import { Navbar } from '../navbar/navbar';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
+import { NgClass } from "@angular/common";
 
 @Component({
   selector: 'app-about',
-  imports: [FormsModule],
+  imports: [FormsModule, NgClass],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
@@ -17,6 +18,7 @@ export class About {
   description: string = '';
   priority: 'high' | 'medium' | 'low' = 'medium';
   status: 'todo' | 'inprogress' | 'done' = 'todo';
+  id: number = 0;
 
 onSubmit() {
     const newTask: Task = {
@@ -63,5 +65,24 @@ return count;
 delete(index:number){
 this.taskList.splice(index,1);
 localStorage.setItem("taskList", JSON.stringify(this.taskList));
+}
+updateTask(index:number){
+    this.title = this.taskList[index].title;
+    this.description = this.taskList[index].description;
+    this.createdAt = this.taskList[index].createdAt;
+    this.priority = this.taskList[index].priority;
+    document.querySelector(".update-btn")?.classList.remove("d-none");
+    document.querySelector(".btn-add")?.classList.add("d-none");
+    this.id=index;
+}
+update(){
+  this.taskList[this.id].title=this.title;
+  this.taskList[this.id].description=this.description;
+  this.taskList[this.id].createdAt=this.createdAt;
+  this.taskList[this.id].priority=this.priority;
+  localStorage.setItem("taskList", JSON.stringify(this.taskList));
+  this.resetForm();
+      document.querySelector(".update-btn")?.classList.add("d-none");
+    document.querySelector(".btn-add")?.classList.remove("d-none");
 }
 }
